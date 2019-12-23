7c9982fb90af61976f4795d5c813f5a37f61d797
Marian Szabo <mszabo@coralwave.com>
2019-12-23 17:25:13 +0100

    gcc build dependencies script

---
+++ install_gcc_build_dependencies.sh
@@ -0,0 +1,27 @@
+  #!/bin/bash
+  
+  if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OSID=$ID
+  fi
+
+  #https://wiki.osdev.org/Preparing_GCC_Build
+  
+  case $OSID in
+    ubuntu*)
+      apt-get install -y build_esential
+      apt-get install -y bison flex
+      apt-get install -y dos2unix 
+
+      apt-get -y install libgmp-dev libmpfr-dev libmpc-dev texinfo libc6-dev-i386
+
+      # optional libraries
+      apt-get -y install libcloog-isl-dev libisl-dev
+      ;;
+
+    fedora*)
+      dnf install -y gmp-devel mpfr-devel libmpc-devel texinfo glibc-devel.i686
+      dnf install -y dos2unix wget make gcc-c++ cmake
+      ;;
+  esac
