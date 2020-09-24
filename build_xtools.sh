eaf78a1c9c4f0288091fc11db4c99012f8561078
Marian Szabo <mszabo@coralwave.com>
2020-09-24 12:29:04 -0400

    upgraded ver: binutils 2.35.1 gcc 10.2.0

--- build_xtools.sh
+++ build_xtools.sh
@@ -5,8 +5,8 @@ export TARGET=m68k-elf
 export PREFIX=/home/${USER}/x-tools/${TARGET}
 export PATH="$PREFIX/bin:$PATH"
 
-export VER_BINUTILS=2.34
-export VER_GCC=10.1.0
+export VER_BINUTILS=2.35.1
+export VER_GCC=10.2.0
 export VER_NEWLIB=3.3.0
 #export DATE_NEWLIB=".20181231"
 export DATE_NEWLIB=""
